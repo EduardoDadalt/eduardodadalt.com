b1cbe8960cbf104705ac2c0dc71d7671251ab732
Eduardo Dadalt <eduardodadalt1@gmail.com>
2024-02-17 19:49:58 -0300

    Add AlertCircle component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import ProgrammingLanguages from "@/components/sections/programming-languages";
 import MyInfo from "@/components/sections/my-info";
 import "./homepage.css";
 import Technologies from "@/components/sections/technology";
+import { AlertCircle } from "lucide-react";
 
 export const revalidate = 86400;
 
 export default function Home() {
   return (
     <div>
-      <div className="bg-amber-300 p-16 text-2xl text-amber-950">
-        Página em construção
+      <div className="flex flex-row items-center justify-center gap-2 bg-amber-300 p-16 text-2xl text-amber-950">
+        <AlertCircle /> Página em construção
       </div>
       <MyInfo />
       {/* Linguagens de Programação */}
