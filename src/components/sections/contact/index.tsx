e27a54688c205b1bcb515a73a4f5b11e25bcec28
Eduardo Dadalt <eduardodadalt1@gmail.com>
2024-02-27 22:08:45 -0300

    Remove unnecessary CSS class from contact section

--- src/components/sections/contact/index.tsx
+++ src/components/sections/contact/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default async function Contact({ locale }: { locale: string }) {
   const dictionary = await getDictionary(locale);
   return (
-    <section className="flex min-h-60 bg-primary-600 py-10 text-slate-50">
+    <section className="min-h-60 bg-primary-600 py-10 text-slate-50">
       <h2 className="text-2xl">{dictionary.homepage.contact.title}</h2>
       <div className="flex items-center gap-2">
         <p>{dictionary.homepage.contact.youCanContactMeVia}</p>
